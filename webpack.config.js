35b9afcfb664d26eacc49f40124461c1ae302ee3
tingtaox <tingtaox@tingtaoxs-MacBook-Pro.local>
2017-11-26 23:18:57 -0800

    Integrate scss loader with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,56 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+var extractSass = new ExtractTextPlugin("main.bundle.css");
+
 module.exports = {
-  entry: ["./src/index.tsx"],
-  output: {
-      filename: "bundle.js",
-      path: __dirname + "/dist"
-  },
+    entry: ["./src/index.tsx", "./scss/main.scss"],
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
 
-  resolve: {
-      // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: [".ts", ".tsx", ".js", ".json"]
-  },
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
 
-  module: {
-      rules: [
-          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
-          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
 
-          // {
-          //   // scss / sass loader for webpack
-          //   test: /\.(sass|scss)$/,
-          //   use: ExtractTextPlugin.extract({
-          //     fallback: 'style-loader',
-          //     use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-          //   })
-          // }
-      ]
-  },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [
+                        { loader: "css-loader", options: { sourceMap: true } },
+                        { loader: "sass-loader", options: { sourceMap: true } }
+                    ],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
+            }
+        ]
+    },
 
-  watch: true,
+    devServer: { inline: true },
 
-  plugins: [
-    new ExtractTextPlugin({
-      // define where to save the file
-      filename: 'dist/main.bundle.css',
-      allChunks: true
-    })
-  ],
+    plugins: [
+        extractSass
+    ],
 
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-      "react": "React",
-      "react-dom": "ReactDOM"
-  },
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
 };
